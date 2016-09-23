784d4494126adc7dbc28356e72aa5b47b1ac9566
Alex Chaffee <alex@stinky.com>
2016-09-23 11:45:59 -0700

    rename 05_stock back to stock

--- learn_java/05_stock/Asset.java
+++
@@ -1,5 +0,0 @@
-public interface Asset
-{
-	public String getName();
-	public double getValue();
-}
--- learn_java/05_stock/Future.java
+++
@@ -1,8 +0,0 @@
-public class Future extends Stock
-{
-	public Future(String symbol, double price, int numShares)
-	{
-		super(symbol, price, numShares);
-	}
-}
-
