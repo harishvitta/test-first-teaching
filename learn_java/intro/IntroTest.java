ce7a78d724d3b140a94d6f3d89e59bd283a24294
Emily Platzer <emilyplatzer@gmail.com>
2016-09-23 10:11:11 -0600

    Removed test (students are supposed to create this test)

--- learn_java/intro/IntroTest.java
+++ learn_java/intro/IntroTest.java
@@ -14,12 +14,6 @@ public class IntroTest extends TestCase
         assertEquals("Goodbye", x.sayGoodbye());
     }
 
-    public void testHowAreYou()
-    {
-        Intro x = new Intro();
-        assertEquals("Fine", x.sayHowAreYou());
-    }
-
 
 }
 
