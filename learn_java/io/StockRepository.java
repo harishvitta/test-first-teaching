0ef95e53aee61b1919d568c5edf9e27fa9ce8218
Alex Chaffee <alex@stinky.com>
2016-09-25 12:50:11 -0700

    fix duplicate StockRepository error

--- learn_java/io/StockRepository.java
+++ learn_java/io/StockRepository.java
@@ -2,13 +2,14 @@
 import java.io.*;
 import java.util.List;
 
-public interface StockRepository
-{
-    public void clear() throws IOException;
-    public int countStocks() throws IOException;
-    public Stock createStock(String symbol, double pricePerShare, int numShares) throws IOException;
-    public void saveStock(Stock stock) throws IOException;
-    public Stock loadStock(long id) throws IOException;
-    public List findAllStocks() throws IOException;
-    public void close() throws IOException;
-}
+// also defined in stock/ dir
+//public interface StockRepository
+//{
+//    public void clear() throws IOException;
+//    public int countStocks() throws IOException;
+//    public Stock createStock(String symbol, double pricePerShare, int numShares) throws IOException;
+//    public void saveStock(Stock stock) throws IOException;
+//    public Stock loadStock(long id) throws IOException;
+//    public List findAllStocks() throws IOException;
+//    public void close() throws IOException;
+//}
